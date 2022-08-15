2a47b972e85bca55a6c8e4a4f68ead32f56c3496
dereksnyder42v2 <dereksnyder42@gmail.com>
2022-08-15 17:12:45 -0600

    what. in. the fuck

--- src/main/java/hello/Hello.java
+++ src/main/java/hello/Hello.java
@@ -17,7 +17,7 @@ public class Hello {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("hello world!");
+		System.out.println("Hello.java: hello world!");
 		doLogStuff();
 	}
 
---
+++ src/main/java/logging/Logger.java
@@ -0,0 +1,41 @@
+package logging;
+
+import org.apache.logging.log4j.LogManager;
+// import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.spi.AbstractLogger;
+
+// abstract class AbstractLoggerC implements org.apache.logging.log4j.Logger {
+
+//     public static void main(String[] args) {
+//         System.out.println("Logger.java: hello world!");
+//     }
+
+//     @Override
+//     public void warn(String message, Object... params) {
+//         ;
+//     }
+// }
+
+public class Logger implements org.apache.logging.log4j.Logger {
+
+    public Logger() {
+        ;
+    }
+    public void someMethod() {
+        ;
+    }
+    @Override
+    public void void warn(String message,
+        Object p0,
+        Object p1,
+        Object p2,
+        Object p3,
+        Object p4,
+        Object p5,
+        Object p6,
+        Object p7,
+        Object p8,
+        Object p9) {
+        ;
+    }
+}
